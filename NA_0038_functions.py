a3b1e32a450730c04b4f494411182166be1ee772
juespega <juespega666@gmail.com>
2023-06-13 23:15:47 -0500

    functions

--- NA_0038_functions.py
+++ NA_0038_functions.py
@@ -217,10 +217,23 @@ El asignar un valor hace que la función cree su propia variable.
 
 # Cómo interactúa la función con sus argumentos
 
+# def my_function(my_list_1):
+#     print("Print #1:", my_list_1)
+#     print("Print #2:", my_list_2)
+#     my_list_1 = [0, 1]
+#     print("Print #3:", my_list_1)
+#     print("Print #4:", my_list_2)
+
+
+# my_list_2 = [2, 3]
+# my_function(my_list_2)
+# print("Print #5:", my_list_2)
+
+
 def my_function(my_list_1):
     print("Print #1:", my_list_1)
     print("Print #2:", my_list_2)
-    my_list_1 = [0, 1]
+    del my_list_1[0]  # Presta atención a esta línea.
     print("Print #3:", my_list_1)
     print("Print #4:", my_list_2)
 
